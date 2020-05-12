85f5813d0c2d9965607928cd3f20343feb6c8658
root <root@ultra.localdomain>
2020-05-12 13:14:24 -0400

    remove unused endpoints

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 var compression = require('compression')
 const express = require('express')
 var Redis = require('ioredis')
+const port = process.env.PORT
 var redis = new Redis(process.env.REDIS_URL)
 const app = express()
 app.use(compression())
-const port = process.env.PORT
 
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
 app.disable('x-powered-by')
 
 app.post('/rest/garage', async (req, res) => {
-    console.log(`POST: got a call to /rest/garage`)
-    console.log(req.body.action)
+    console.log(`POST: got a call to /rest/garage action: ${req.body.action}`)
     await redis.set("garage", req.body.action)
     res.sendStatus(200)
 })
@@ -27,21 +26,8 @@ app.get('/rest/garage', async (req, res) => {
     res.json(response)
 })
 
-app.post('/rest/geofence', async (req, res) => {
-    console.log(`POST: got a call to /rest/geofence`)
-    console.log(req.body)
-    await redis.set("location", req.body)
-    res.sendStatus(200)
-})
-
-app.get('/rest/geofence', async (req, res) => {
-    console.log(`GET: got a call to /rest/geofence`)
-    var value = await redis.get("location")
-    res.send(value)
-})
-
 app.get('/rest/', async (req, res) => {
     console.log(`got a call to /rest/`)
     res.send("Nothing to see here.")
 })
-app.listen(port, () => console.log(`IfTTT Hook App Listening...`))
+app.listen(port, () => console.log(`Garage API Listening...`))
