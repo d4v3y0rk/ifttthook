241ca3e2d1027b26155339fdb60ed33da04e91ec
Dave York <dave.york@me.com>
2020-05-12 01:39:27 -0400

    add redis and write out req

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const client = require('redis').createClient(process.env.REDIS_URL)
 const port = process.env.PORT
 
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    console.log(req.body)
+    console.log(req)
     res.sendStatus(200)
 })
 
