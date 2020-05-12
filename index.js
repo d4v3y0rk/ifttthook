6e6a6202904ac94ca12f35b6266bdc46a7a5f310
Dave York <dave.york@me.com>
2020-05-12 01:56:50 -0400

    change the name of the client

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require('express')
 const redis = require("redis")
 const app = express()
-const client = redis.createClient(process.env.REDIS_URL)
+const redisClient = redis.createClient(process.env.REDIS_URL)
 const port = process.env.PORT
 
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/toggle', async (req, res) => {
-    await client.set("garage", "toggle", redis.print)
+    await redisClient.set("garage", "toggle", redis.print)
     res.sendStatus(200)
 })
 
 app.get('/', async (req, res) => {
     if (client.get('garage') == "toggle") {
-        console.log(client.get('garage'))
+        console.log(redisClient.get('garage'))
         res.send(`Found the key...`)
     } else {
         res.send(`did not find the key.`)
     }
-    await client.set('garage', '', redis.print)
+    await redisClient.set('garage', '', redis.print)
     res.sendStatus(200)
 })
 app.listen(port, () => console.log(`IfTTT Hook App Listening...`))
