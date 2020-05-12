8d17a0bb31aa0e6527298d8bcb46cec34cb4555c
Dave York <dave.york@me.com>
2020-05-12 01:45:33 -0400

    use redis

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const port = process.env.PORT
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    console.log(req)
+app.get('/toggle', (req, res) => {
+    client.set("garage", "toggle", redis.print)
     res.sendStatus(200)
 })
 
+app.get('/', (req, res) => {
+    console.log(client.get('garage'))
+    client.set('garage', '', redis.print)
+    res.sendStatus(200)
+})
 app.listen(port, () => console.log(`IfTTT Hook App Listening...`))
